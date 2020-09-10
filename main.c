a0b8aead3b1ecf0102422e963c44db2bea39c510
EricBenditt <erb5623@psu.edu>
2020-09-10 15:11:14 +0000

    I shifted my if else if statements

--- main.c
+++ main.c
@@ -12,32 +12,32 @@ int main(void) {
   printf("Enter your CMPSC 131 grade: ");
   scanf("%d", &number);
 
-  // true if number is greater than or equal to 93
-    if (number >=93.0) {
+  
+  if (number >=93.0) {
       printf("Your letter grade for CMPSC 131 is A."); 
     }
-    else if (number >=90.0) {
+  else if (number >=90.0) {
       printf("Your letter grade for CMPSC 131 is A-.");
     }
-    else if (number >=87.0) {
+  else if (number >=87.0) {
       printf("Your letter grade for CMPSC 131 is B+.");
     }
-    else if (number >=83.0) {
+  else if (number >=83.0) {
       printf("Your letter grade for CMPSC 131 is B.");
     }
-    else if (number >=80.0) {
+  else if (number >=80.0) {
       printf("Your letter grade for CMPSC is B-.");
     }
-    else if (number >=77.0) {
+  else if (number >=77.0) {
       printf("Your letter grade for CMPSC 131 is C+.");
     }
-    else if (number >=70.0) {
+  else if (number >=70.0) {
       printf("Your letter grade for CMPSC 131 is C.");
     }
-    else if (number >=60.0) {
+  else if (number >=60.0) {
       printf("Your letter grade for CMPSC 131 is D.");
     }
-    else if (number <60.0) {
+  else if (number <60.0) {
       printf("Your letter grade for CMPSC is F."); 
     }
   return 0;
